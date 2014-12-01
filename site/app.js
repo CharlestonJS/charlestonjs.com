5d4fdc12c709d0307904c7c452508bd85a499654
Andre Dublin <81dublin@gmail.com>
2014-12-01 11:04:59 -0500

    Update playbooks Update site

--- site/app.js
+++ site/app.js
@@ -8,13 +8,11 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-/**
- * Route Imports
- */
-var signup = require('./routes/signup');
-
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,18 +20,23 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-/**
- * Development Settings
- */
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
 if (app.get('env') === 'development') {
-    // This will change in production since we'll be using the dist folder
-    app.use(express.static(path.join(__dirname, '../client')));
-    // This covers serving up the index page
-    app.use(express.static(path.join(__dirname, '../client/.tmp')));
-    app.use(express.static(path.join(__dirname, '../client/app')));
-
-    // Error Handling
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
@@ -43,28 +46,15 @@ if (app.get('env') === 'development') {
     });
 }
 
-/**
- * Production Settings
- */
-if (app.get('env') === 'production') {
-
-    // changes it to use the optimized version for production
-    app.use(express.static(path.join(__dirname, '/dist')));
-
-    // production error handler
-    // no stacktraces leaked to user
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: {}
-        });
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
     });
-}
+});
 
-/**
- * Routes
- */
-app.use('/signup', signup);
 
 module.exports = app;
