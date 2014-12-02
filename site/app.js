fee10cd76c0247f883268d2e0bdde32b72fa744c
Andre Dublin <81dublin@gmail.com>
2014-12-02 16:25:01 -0500

    favicon include bower components
    Signed-off-by: Andre Dublin <81dublin@gmail.com>

--- site/app.js
+++ site/app.js
@@ -29,12 +29,13 @@ module.exports = function () {
   app.set('view engine', 'jade');
 
   // uncomment after placing your favicon in /public
-  //app.use(favicon(__dirname + '/public/favicon.ico'));
+  app.use(favicon(__dirname + '/public/favicon.ico'));
   app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: false}));
   app.use(cookieParser());
   app.use(express.static(path.join(__dirname, 'public')));
+  app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
   app.use('/', routes);
 
