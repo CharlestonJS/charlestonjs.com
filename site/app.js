fde7312ea31b6314fbcca8d8dc20308750c0c460
Andre Dublin <81dublin@gmail.com>
2014-12-02 15:07:40 -0500

    use harmony flag
    Signed-off-by: Andre Dublin <81dublin@gmail.com>

--- site/app.js
+++ site/app.js
@@ -1,60 +1,73 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+module.exports = function () {
+  'use strict';
+  const express = require('express');
+  const path = require('path');
+  const favicon = require('serve-favicon');
+  const logger = require('morgan');
+  const cookieParser = require('cookie-parser');
+  const bodyParser = require('body-parser');
+  const fs = require('fs');
+  //const router = express.Router();
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+  const routes = require('./routes/index');
 
-var app = express();
+  //fs.readdirSync(__dirname + '/routes', function (err, files) {
+  //  if (err) {
+  //    return;
+  //  }
+  //
+  //  files.forEach(function (file) {
+  //    let fileName = file.substr(0, file.indexOf('.'));
+  //    require('./routes/', + fileName)(router);
+  //  });
+  //});
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+  const app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-app.use('/', routes);
-app.use('/users', users);
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(__dirname + '/public/favicon.ico'));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+  app.use('/', routes);
+
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    let err = new Error('Not Found');
     err.status = 404;
     next(err);
-});
+  });
 
-// error handlers
+  // error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
     });
-}
+  }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
-        message: err.message,
-        error: {}
+      message: err.message,
+      error: {}
     });
-});
+  });
 
-
-module.exports = app;
+  return app;
+};
