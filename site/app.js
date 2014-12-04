4f39df6972560a38dfbdd3eaac8b406ae88fc81c
Andre Dublin <81dublin@gmail.com>
2014-12-04 11:53:30 -0500

    use var
    Signed-off-by: Andre Dublin <81dublin@gmail.com>

--- site/app.js
+++ site/app.js
@@ -46,7 +46,7 @@ module.exports = function () {
 
   // catch 404 and forward to error handler
   app.use(function (req, res, next) {
-    let err = new Error('Not Found');
+    var err = new Error('Not Found');
     err.status = 404;
     next(err);
   });
