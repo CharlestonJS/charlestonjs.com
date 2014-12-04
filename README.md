616197b1caa6053a88e913bb06fd78b014e90556
Andre Dublin <81dublin@gmail.com>
2014-12-04 12:44:48 -0500

    update readme, use global form of use strict
    Signed-off-by: Andre Dublin <81dublin@gmail.com>

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ INSTALLATION:
 1. Download & install [virtualbox](https://www.virtualbox.org/)
 2. Download & install [vagrant](https://www.vagrantup.com/)
 3. Clone the repo
-4. Cd into the repo directory and no first run, run the cmd `vagrant up --provision-with ansible`
+4. Cd into the repo directory and no first run, run the cmd `vagrant up --provision` or `vagrant reload --provision`
 5. Browse to http://localhost:8080
 6. Run `vagrant` for additional commands
--- site/app.js
+++ site/app.js
@@ -1,79 +1,78 @@
-module.exports = function () {
-  'use strict';
-  var express = require('express');
-  var path = require('path');
-  var favicon = require('serve-favicon');
-  var logger = require('morgan');
-  var cookieParser = require('cookie-parser');
-  var bodyParser = require('body-parser');
-  var fs = require('fs');
-  //const router = express.Router();
+'use strict';
 
-  var routes = require('./routes/index');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+//const router = express.Router();
 
-  //fs.readdirSync(__dirname + '/routes', function (err, files) {
-  //  if (err) {
-  //    return;
-  //  }
-  //
-  //  files.forEach(function (file) {
-  //    let fileName = file.substr(0, file.indexOf('.'));
-  //    require('./routes/', + fileName)(router);
-  //  });
-  //});
+var routes = require('./routes/index');
 
-  var app = express();
+//fs.readdirSync(__dirname + '/routes', function (err, files) {
+//  if (err) {
+//    return;
+//  }
+//
+//  files.forEach(function (file) {
+//    let fileName = file.substr(0, file.indexOf('.'));
+//    require('./routes/', + fileName)(router);
+//  });
+//});
 
-  // view engine setup
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'jade');
+var app = express();
 
-  // uncomment after placing your favicon in /public
-  app.use(favicon(__dirname + '/public/favicon.ico'));
-  app.use(logger('dev'));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({extended: false}));
-  app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-  if (app.get('env') === 'development') {
-    app.use(require('connect-livereload')({
-      port: 35729
-    }));
-  }
+// uncomment after placing your favicon in /public
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-  app.use('/', routes);
+if (app.get('env') === 'development') {
+  app.use(require('connect-livereload')({
+    port: 35729
+  }));
+}
 
-  // catch 404 and forward to error handler
-  app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
+app.use('/', routes);
 
-  // error handlers
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-  // development error handler
-  // will print stacktrace
-  if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-      res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: err
-      });
-    });
-  }
+// error handlers
 
-  // production error handler
-  // no stacktraces leaked to user
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
   app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: {}
+      error: err
     });
   });
+}
 
-  return app;
-};
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
