997baa2e478d273fdc129e7c2f03148debce04a2
Andre Dublin <81dublin@gmail.com>
2014-12-05 23:03:25 -0500

    home page controller
    Signed-off-by: Andre Dublin <81dublin@gmail.com>

--- site/routes/index.js
+++ site/routes/index.js
@@ -1,8 +1,10 @@
+var homeCtrl = require('./controllers/home');
+
 module.exports = function (router) {
   'use strict';
   /* GET home page. */
   router.route('/')
     .get(function(req, res) {
-      res.render('index', {title: 'Express'});
+      homeCtrl.index(req, res);
     });
 };
