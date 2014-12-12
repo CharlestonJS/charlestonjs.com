7fc6a5340921e923ca5f0c00da11ae67e88688e3
Andre Dublin <81dublin@gmail.com>
2014-12-11 22:39:45 -0500

    one var statement
    Signed-off-by: Andre Dublin <81dublin@gmail.com>

--- site/app.js
+++ site/app.js
@@ -1,16 +1,15 @@
 'use strict';
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var livereload = require('livereload');
-var router = express.Router();
-
-var app = express();
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    fs = require('fs'),
+    livereload = require('livereload'),
+    router = express.Router(),
+    app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
