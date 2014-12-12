84988687f753b2068c73cef0701567ec639b0507
Andre Dublin <81dublin@gmail.com>
2014-12-11 22:37:01 -0500

    amend last commit
    Signed-off-by: Andre Dublin <81dublin@gmail.com>

--- site/routes/site.js
+++ site/routes/site.js
@@ -1,9 +1,9 @@
-var homeCtrl = require('./../controllers/site');
+var siteCtrl = require('./../controllers/site');
 
 module.exports = function (router) {
   'use strict';
   router.route('/').get(function (req, res) {
-    homeCtrl.index(req, res);
+    siteCtrl.index(req, res);
   });
 
   router.route('/login').get(function (req, res) {
